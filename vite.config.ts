f514c2f328da02aa9f96909ddcbea83bdb74d8dd
Juan Herrera <73452788+TantoG@users.noreply.github.com>
2025-12-28 22:50:09 -0300

    feat: configure project for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
         return {
-            base: '/Tokens_Didacticos_Nivel_INICIAL/',
+            base: '/tokens3/',
             build: {
                 outDir: 'docs'
             },
